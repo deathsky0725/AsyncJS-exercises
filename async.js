427cc8749ea9816bef615a417877e41f9c8d4ca7
Deathsky <wbenjapol@gmail.com>
2017-11-21 10:32:49 +0700

    'done'

--- async.js
+++ async.js
@@ -1,7 +1,9 @@
 const request = require('request');
-
+const callback = ()=>{
+    console.log('Response');
+};
 const test = () => {
-  
+  request('http://www.google.com', callback);
 }
 //START OF PROGRAM
 console.log("Hitting google");
